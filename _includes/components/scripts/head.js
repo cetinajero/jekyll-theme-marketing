1b14d3c169eee0feac07f138c4cf5781246a1c4e
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2020-06-10 17:59:27 -0600

    yarn unplug @fortawesome
    Also updated to new path

--- _includes/components/scripts/head.js
+++ _includes/components/scripts/head.js
@@ -1,7 +1,7 @@
-<script src="{{ '/assets/node_modules/@fortawesome/fontawesome-free-brands/index.js' | relative_url }}"></script>
-<script src="{{ '/assets/node_modules/@fortawesome/fontawesome-free-regular/index.js' | relative_url }}"></script>
-<script src="{{ '/assets/node_modules/@fortawesome/fontawesome-free-solid/index.js' | relative_url }}"></script>
-<script src="{{ '/assets/node_modules/@fortawesome/fontawesome/index.js' | relative_url }}"></script>
+<script src="{{ '/assets/node_modules/@fortawesome-fontawesome-free-brands-npm-5.0.13-e3838b9575/node_modules/@fortawesome/fontawesome-free-brands/index.js' | relative_url }}"></script>
+<script src="{{ '/assets/node_modules/@fortawesome-fontawesome-free-regular-npm-5.0.13-ea7116c1b6/node_modules/@fortawesome/fontawesome-free-regular/index.js' | relative_url }}"></script>
+<script src="{{ '/assets/node_modules/@fortawesome-fontawesome-free-solid-npm-5.0.13-dde3cd7819/node_modules/@fortawesome/fontawesome-free-solid/index.js' | relative_url }}"></script>
+<script src="{{ '/assets/node_modules/@fortawesome-fontawesome-npm-1.1.8-5d2e21a3ed/node_modules/@fortawesome/fontawesome/index.js' | relative_url }}"></script>
 <script src="{{ '/assets/node_modules/jquery/dist/jquery.min.js' | relative_url }}"></script>
 
 <script src="{{ '/assets/vendors/revolution-slider/js/jquery.themepunch.tools.min.js' | relative_url }}" type="text/javascript"></script>
