175be68e793981146938d85b84fe69342a0b7fe5
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2020-06-11 06:56:22 -0600

    yarn unplug jquery
    Also updated to new path

--- _includes/components/scripts/head.js
+++ _includes/components/scripts/head.js
@@ -2,7 +2,7 @@
 <script src="{{ '/assets/node_modules/@fortawesome-fontawesome-free-regular-npm-5.0.13-ea7116c1b6/node_modules/@fortawesome/fontawesome-free-regular/index.js' | relative_url }}"></script>
 <script src="{{ '/assets/node_modules/@fortawesome-fontawesome-free-solid-npm-5.0.13-dde3cd7819/node_modules/@fortawesome/fontawesome-free-solid/index.js' | relative_url }}"></script>
 <script src="{{ '/assets/node_modules/@fortawesome-fontawesome-npm-1.1.8-5d2e21a3ed/node_modules/@fortawesome/fontawesome/index.js' | relative_url }}"></script>
-<script src="{{ '/assets/node_modules/jquery/dist/jquery.min.js' | relative_url }}"></script>
+<script src="{{ '/assets/node_modules/jquery-npm-3.5.0-6db13a3f79/node_modules/jquery/dist/jquery.min.js' | relative_url }}"></script>
 
 <script src="{{ '/assets/vendors/revolution-slider/js/jquery.themepunch.tools.min.js' | relative_url }}" type="text/javascript"></script>
 <script src="{{ '/assets/vendors/revolution-slider/js/jquery.themepunch.revolution.min.js' | relative_url }}" type="text/javascript"></script>
