38ce3a9d29ead60fc0188df295a03fc4c9162750
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2020-06-13 08:56:38 -0600

    Make the catalog images height responsible

--- _includes/components/product-cards/product-details.js
+++ _includes/components/product-cards/product-details.js
@@ -1,15 +1,28 @@
-var img = new Image()
-img.src = '{{ site.amazon-s3 }}{{ resource }}'
+const images = Array.from(
+    document
+            .getElementById("product-details-footer")
+            .getElementsByTagName('div')
+)
 
-dummy = document
-    .getElementById("product-details-footer")
-    .getElementsByTagName('img')[{{ forloop.index0 }}]
-
-async function updateHeight() {
+async function updateHeight(image) {
     do {
+        var dummy = image.getElementsByTagName('img')[0]
+        var img = new Image()
+        img.src = image.style.backgroundImage.split('"')[1]
+
         await new Promise(r => setTimeout(r, 1000));
-        dummy.style.height = `${img.naturalHeight}px`
+        height = img.naturalHeight * dummy.clientWidth / 800
+        dummy.style.height = `${height}px`
+        console.log(`${dummy.clientWidth}px x ${height}px`)
     } while (img.naturalHeight < 1)
 }
 
-updateHeight()
+function updateCatalogImagesHeight() {
+    images.forEach(image => 
+        updateHeight(image)
+    );
+}
+
+window.addEventListener('resize', updateCatalogImagesHeight)
+
+updateCatalogImagesHeight()
