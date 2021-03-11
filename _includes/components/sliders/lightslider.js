499078da702c9526f285adee450adcb0034de526
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2021-03-11 14:01:05 -0700

    Implement responsive configuration for mobiles

--- _includes/components/sliders/lightslider.js
+++ _includes/components/sliders/lightslider.js
@@ -16,15 +16,26 @@ jQuery('#lightSlider').lightSlider({
 window.addEventListener('resize',initElevateZoom)
 
 function initElevateZoom() {
-  img = jQuery('#lightSlider').find('img')
+  const images = jQuery('#lightSlider').find('img')
+  const windowWidth = window.innerWidth
+  const imgNatWidth = images[0].naturalWidth
 
-  const minNatWidth = 700
-  const natWidth = (img[0].naturalWidth)
+  const minImgNatWidth = 700
 
-  if (natWidth >= minNatWidth) {
-    img.elevateZoom({
-      zoomWindowFadeIn: 500,
-      zoomWindowFadeOut: 500
-    })
+  const desktopOptions = {
+    zoomWindowFadeIn: 500,
+    zoomWindowFadeOut: 500
+  }
+
+  const mobileOptions = {
+    zoomType: 'inner',
+    cursor: 'crosshair'
+  }
+
+  if (imgNatWidth >= minImgNatWidth) {
+    if (windowWidth < 768) 
+      images.elevateZoom(mobileOptions)
+    else
+      images.elevateZoom(desktopOptions)
   }
 }
