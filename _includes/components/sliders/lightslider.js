5d1b5845b943d40c461776e7cf569005871675da
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2021-03-11 10:32:21 -0700

    Move HTML script tags to liquid

--- _includes/components/sliders/lightslider.js
+++ _includes/components/sliders/lightslider.js
@@ -1,19 +1,17 @@
-<script type="text/javascript">
-  jQuery('#lightSlider').lightSlider({
-    gallery: true,
-    item: 1,
-    loop: true,
-    slideMargin: 10,
-    thumbItem: 4,
+jQuery('#lightSlider').lightSlider({
+  gallery: true,
+  item: 1,
+  loop: true,
+  slideMargin: 10,
+  thumbItem: 4,
 
-    galleryMargin: 5,
-    thumbMargin: 5,
+  galleryMargin: 5,
+  thumbMargin: 5,
 
-    onSliderLoad: function (el) {
-      el.find('img').elevateZoom({
-        zoomWindowFadeIn: 500,
-        zoomWindowFadeOut: 500
-      })
-    }
-  });
-</script>
+  onSliderLoad: function (el) {
+    el.find('img').elevateZoom({
+      zoomWindowFadeIn: 500,
+      zoomWindowFadeOut: 500
+    })
+  }
+});
