0c3c8d4cfcd147280f3e6203430abf1e3c74a704
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2021-04-28 17:05:38 -0600

    Create cart-item info-card component

--- _includes/components/shopping-carts/vanilla-cart-js/cart.js
+++ _includes/components/shopping-carts/vanilla-cart-js/cart.js
@@ -14,17 +14,7 @@ if (cart.length > 0) {
     const product = cartItem
 
     cartDOM.insertAdjacentHTML('beforeend', `
-      <div class="cart-item-${product.model}">
-        <img class="cart-item-image" src="${product.image}" alt="${product.model}">
-        <h3 class="cart-item-model">${product.model}</h3>
-        <h3 class="cart-item-brand">${product.brand}</h3>
-        <h3 class="cart-item-description">${product.description}</h3>
-        <h3 class="cart-item-price">${product.price}</h3>
-        <button class="btn btn-primary btn-small${(product.quantity === 1 ? ' btn-danger' : '')}" data-action="decreaseItem">&minus;</button>
-        <h3 class="cart-item-quantity">${product.quantity}</h3>
-        <button class="btn btn-primary btn-small" data-action="increaseItem">&plus;</button>
-        <button class="btn btn-danger btn-small" data-action="removeItem">&times;</button>
-      </div>
+      {% include components/info-cards/cart-item.html %}
     `)
 
     const cartItemDOM = cartDOM.querySelector(`.cart-item-${product.model}`)
