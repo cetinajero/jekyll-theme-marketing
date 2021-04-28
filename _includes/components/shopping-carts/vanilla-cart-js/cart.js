1784764e8f25be9755cc51ed569d4c3cfaade91b
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2021-04-28 13:22:40 -0600

    Create decrease quantity and remove items actions

--- _includes/components/shopping-carts/vanilla-cart-js/cart.js
+++ _includes/components/shopping-carts/vanilla-cart-js/cart.js
@@ -3,6 +3,11 @@
 let cart = (JSON.parse(localStorage.getItem('cart')) || [])
 const cartDOM = document.querySelector('.cart')
 
+function removeItem(product, cartItemDOM) {
+  setTimeout(() => cartItemDOM.remove(), 250)
+  cart = cart.filter(cartItem => cartItem.model !== product.model)
+}
+
 if (cart.length > 0) {
   cart.forEach(cartItem => {
     const product = cartItem
@@ -14,7 +19,7 @@ if (cart.length > 0) {
         <h3 class="cart-item-brand">${product.brand}</h3>
         <h3 class="cart-item-description">${product.description}</h3>
         <h3 class="cart-item-price">${product.price}</h3>
-        <button class="btn btn-primary btn-small" data-action="decreaseItem">&minus;</button>
+        <button class="btn btn-primary btn-small btn-danger" data-action="decreaseItem">&minus;</button>
         <h3 class="cart-item-quantity">${product.quantity}</h3>
         <button class="btn btn-primary btn-small" data-action="increaseItem">&plus;</button>
         <button class="btn btn-danger btn-small" data-action="removeItem">&times;</button>
@@ -22,11 +27,37 @@ if (cart.length > 0) {
     `)
 
     const cartItemDOM = cartDOM.querySelector(`.cart-item-${product.model}`)
+
     cartItemDOM.querySelector('[data-action="increaseItem"]').addEventListener('click', () => {
       cart.forEach(cartItem => {
         if (cartItem.model === product.model) {
           cartItemDOM.querySelector('.cart-item-quantity').innerText = ++cartItem.quantity
+          cartItemDOM.querySelector('[data-action="decreaseItem"]').classList.remove('btn-danger')
           localStorage.setItem('cart', JSON.stringify(cart))
+        }
+      })
+    })
+
+    cartItemDOM.querySelector('[data-action="decreaseItem"]').addEventListener('click', () => {
+      cart.forEach(cartItem => {
+        if (cartItem.model === product.model) {
+          if (cartItem.quantity > 1) {
+            cartItemDOM.querySelector('.cart-item-quantity').innerText = --cartItem.quantity
+          } else {
+            removeItem(product, cartItemDOM)
+          }
+
+          if (cartItem.quantity === 1) {
+            cartItemDOM.querySelector('[data-action="decreaseItem"]').classList.add('btn-danger')
+          }
+        }
+      })
+    })
+
+    cartItemDOM.querySelector('[data-action="removeItem"]').addEventListener('click', () => {
+      cart.forEach(cartItem => {
+        if (cartItem.model === product.model) {
+          removeItem(product, cartItemDOM)
         }
       })
     })
