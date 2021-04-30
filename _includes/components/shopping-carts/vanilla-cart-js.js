a3531b2239e1dc1ed9406760b2aec5983ee9d834
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2021-04-30 11:23:00 -0600

    Disable add to card button when product is added to cart

--- _includes/components/shopping-carts/vanilla-cart-js.js
+++ _includes/components/shopping-carts/vanilla-cart-js.js
@@ -9,7 +9,8 @@ addToCartButton.addEventListener('click', () => {
     brand: document.querySelector('.product-brand strong').innerText,
     image: document.querySelectorAll('.product-images img')[1].getAttribute('src'),
     price: parseFloat(document.querySelector('.product-price').innerText.substring(1)),
-    description: document.querySelector('.product-description').innerText
+    description: document.querySelector('.product-description').innerText,
+    quantity: 1
   }
 
   const isInCart = (cart.filter(cartItem => (cartItem.model === product.model)).length > 0);
@@ -17,5 +18,6 @@ addToCartButton.addEventListener('click', () => {
   if (!isInCart) {
     cart.push(product)
     addToCartButton.innerText = 'In Cart'
+    addToCartButton.classList.add('disabled')
   }
 })
