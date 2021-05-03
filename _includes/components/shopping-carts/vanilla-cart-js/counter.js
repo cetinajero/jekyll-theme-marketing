446719ed63aeaa7a20be97cdeb390a5e991e5502
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2021-05-03 16:29:57 -0600

    Show counter badge also on mobile

--- _includes/components/shopping-carts/vanilla-cart-js/counter.js
+++ _includes/components/shopping-carts/vanilla-cart-js/counter.js
@@ -3,17 +3,20 @@
 let cart = (JSON.parse(localStorage.getItem('cart')) || [])
 
 function updateCounter(count) {
-  const icon = document.querySelector('#shopping-cart-icon')
-  const counter = icon.querySelector('span')
-  const background = icon.querySelector('.fa-circle')
+  const icons = document.querySelectorAll('.shopping-cart-icon')
 
-  if (count > 0) {
-    counter.innerHTML = count
-    background.classList.remove('d-none')
-  } else {
-    counter.innerHTML = ''
-    background.classList.add('d-none')
-  }
+  icons.forEach(icon => {
+    const counter = icon.querySelector('span')
+    const background = icon.querySelector('.fa-circle')
+
+    if (count > 0) {
+      counter.innerHTML = count
+      background.classList.remove('d-none')
+    } else {
+      counter.innerHTML = ''
+      background.classList.add('d-none')
+    }
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
