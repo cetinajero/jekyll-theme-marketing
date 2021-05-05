ea1a9b4501bcb67518a1a562f1e2e3544cd9fb32
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2021-05-05 17:55:00 -0600

    Disable cart on products without price

--- _includes/components/shopping-carts/vanilla-cart-js/product.js
+++ _includes/components/shopping-carts/vanilla-cart-js/product.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const productModel = document.querySelector('.product-model strong').innerText
+const productPrice = parseFloat(document.querySelector('.product-price').innerText.substring(1))
 const isCartEnabled = document.querySelector('.shopping-cart-icon') ? true : false
 const isProductInCart = (cart.filter(i => (i.model === productModel)).length > 0)
 const addToCartButton = document.querySelector('[data-action="addToCart"]')
+const productHasPrice = productPrice > 0 ? true : false
 
 function showCartButton(button) {
   button.classList.remove('d-none')
@@ -18,9 +20,9 @@ function enableCartButton(button) {
   button.addEventListener('click', () => {
     const product = {
       model: productModel,
+      price: productPrice,
       brand: document.querySelector('.product-brand strong').innerText,
       image: document.querySelectorAll('.product-images img')[1].getAttribute('src'),
-      price: parseFloat(document.querySelector('.product-price').innerText.substring(1)),
       description: document.querySelector('.product-description').innerText,
       url: window.location.href,
       quantity: 1
@@ -37,7 +39,7 @@ function enableCartButton(button) {
   })
 }
 
-if (isCartEnabled) {
+if (isCartEnabled && productHasPrice) {
   if (isProductInCart)
     disableCartButton(addToCartButton)
   else
