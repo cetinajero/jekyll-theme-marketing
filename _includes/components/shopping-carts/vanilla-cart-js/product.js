4c453b8aad3adfceb368eb4b9f631ddbd0ea6ac5
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2021-05-05 17:50:00 -0600

    Refactor vanilla-cart product component

--- _includes/components/shopping-carts/vanilla-cart-js/product.js
+++ _includes/components/shopping-carts/vanilla-cart-js/product.js
@@ -1,29 +1,23 @@
 'use strict'
 
+const productModel = document.querySelector('.product-model strong').innerText
 const isCartEnabled = document.querySelector('.shopping-cart-icon') ? true : false
+const isProductInCart = (cart.filter(i => (i.model === productModel)).length > 0)
 const addToCartButton = document.querySelector('[data-action="addToCart"]')
+
+function showCartButton(button) {
+  button.classList.remove('d-none')
+}
 
 function disableCartButton(button) {
   button.innerText = 'In Cart'
   button.classList.add('disabled')
 }
 
-function updateButtonState(button) {
-  if (cart.length > 0) {
-    cart.forEach(cartItem => {
-      if (cartItem.model === document.querySelector('.product-model strong').innerText) {
-        disableCartButton(button)
-      }
-    })
-  }
-}
-
 function enableCartButton(button) {
-  button.classList.remove('d-none')
-
   button.addEventListener('click', () => {
     const product = {
-      model: document.querySelector('.product-model strong').innerText,
+      model: productModel,
       brand: document.querySelector('.product-brand strong').innerText,
       image: document.querySelectorAll('.product-images img')[1].getAttribute('src'),
       price: parseFloat(document.querySelector('.product-price').innerText.substring(1)),
@@ -44,8 +38,12 @@ function enableCartButton(button) {
 }
 
 if (isCartEnabled) {
-  updateButtonState(addToCartButton)
-  enableCartButton(addToCartButton)
+  if (isProductInCart)
+    disableCartButton(addToCartButton)
+  else
+    enableCartButton(addToCartButton)
+
+  showCartButton(addToCartButton)
 } else {
   addToCartButton.remove()
 }
