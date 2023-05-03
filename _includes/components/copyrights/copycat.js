26cc88baadc779740851474946b061c1dd80d67a
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2023-05-03 16:51:45 -0600

    Add staging and LAN to copycat exceptions

--- _includes/components/copyrights/copycat.js
+++ _includes/components/copyrights/copycat.js
@@ -6,6 +6,11 @@ cc[4]=cc[3].origin
 cc[5]=cc[2].head
 cc[6]=cc[5].parentElement
 
-if (cc[4] != '{{ site.url | replace: "0.0.0.0", "localhost" }}') {
+{% capture location %}{{ site.url
+  | replace: "/", "\/"
+  | replace: "www.", "(www.|staging.)"
+  | replace: "0.0.0.0", "localhost"}}{% endcapture %}
+
+if (!cc[4].match(/({{ location }}|http:\/\/172\.31\.3[0-1]\.[0-9]{2}:)/)) {
   cc[6].remove()
 }
